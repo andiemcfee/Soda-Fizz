6d36464884e7ede12f1d012da73d0d27f9556fb7
Andie Nanney <andiemcfee@gmail.com>
2018-06-14 13:52:15 -0500

    Added movement
    Also changed color of earth material

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () 
+    {
+        rigidBody = GetComponent<Rigidbody>(); //important!!
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        ProcessInput();
+	}
+
+    private void ProcessInput()
+    {
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.A) | Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.D) | Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+    }
+}
